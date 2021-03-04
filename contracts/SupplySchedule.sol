b8edd791587a1c60a1509b69b0b112ed24a15af7
Conight <xfddwhh@gmail.com>
2021-03-04 18:13:40 +0800

    Update SupplySchedule.sol

--- contracts/SupplySchedule.sol
+++ contracts/SupplySchedule.sol
@@ -42,7 +42,7 @@ contract SupplySchedule is Owned, ISupplySchedule {
     // How long each inflation period is before mint can be called
     uint public constant MINT_PERIOD_DURATION = 1 weeks;
 
-    uint public constant INFLATION_START_DATE = 1551830400; // 2019-03-06T00:00:00+00:00
+    uint public  INFLATION_START_DATE = 1551830400; // 2019-03-06T00:00:00+00:00
     uint public constant MINT_BUFFER = 1 days;
     uint8 public constant SUPPLY_DECAY_START = 40; // Week 40
     uint8 public constant SUPPLY_DECAY_END = 234; //  Supply Decay ends on Week 234 (inclusive of Week 234 for a total of 195 weeks of inflation decay)
@@ -62,6 +62,13 @@ contract SupplySchedule is Owned, ISupplySchedule {
         weekCounter = _currentWeek;
     }
 
+    function resetLastMintEventAndCurrentWeek(uint _lastMintEvent,uint _currentWeek) public onlyOwner {
+        lastMintEvent = _lastMintEvent;
+        weekCounter = _currentWeek;
+    }
+    function resetInflationStartDate(uint _NEW_INFLATION_START_DATE) public onlyOwner {
+        INFLATION_START_DATE = _NEW_INFLATION_START_DATE;
+    }
     // ========== VIEWS ==========
 
     /**
